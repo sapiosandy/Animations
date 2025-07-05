54b4df582577e0f872d963284cb406e2a80f3432
Sandy Gomez <sgomez1029@gmail.com>
2025-07-05 15:55:53 -0700

    Day 32 - Customizing Animations in SwiftUI

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,16 +13,28 @@ struct ContentView: View {
 
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
-        
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+        .overlay (
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
